65a91f16dda9a9b19f640ab39e720d6fe5e77bf9
Amir Hamdi <am3566146@gmail.com>
2023-03-16 06:43:21 -0700

    wqq

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-*main = Entry point
+*
+*main - Entry point
 *
 *Description: print text
 *
@@ -8,6 +9,6 @@
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
 }
