bdec48c572163268892b134bafd440e120eb3bc2
Amir Hamdi <am3566146@gmail.com>
2023-03-16 06:25:53 -0700

    sww

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main = Entry point
+*
+*Description: print text
+*
+*Return: 0
+*/
+int main(void)
+{
+    char amir[]= "\"Programming is like building a multilingual puzzle";
+    puts(amir);
+    return0;
+}    
