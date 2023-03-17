893d05484d4c3f2752d4da5f8259f9eafe282029
Amir Hamdi <am3566146@gmail.com>
2023-03-17 04:23:51 -0700

    qq

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  the program prints the numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = 0;
+while (c < 10)
+{
+printf("%d", c);
+c++;
+}
+printf("\n");
+
+return (0);
+}
