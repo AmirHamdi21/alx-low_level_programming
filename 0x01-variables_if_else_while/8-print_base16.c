18d5cbf6be79d14206f2dd8c1e794946834f9385
Amir Hamdi <am3566146@gmail.com>
2023-03-17 04:38:03 -0700

    ss

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  the program prints the lowercase alph
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+int i;
+char c = 'a';
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+
+return 0;
+}
