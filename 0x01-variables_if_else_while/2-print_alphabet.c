891a70b6a323d8767cd860df05ed94b906ae516c
Amir Hamdi <am3566146@gmail.com>
2023-03-17 02:11:34 -0700

    wttt

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
 *
 */
 
-int main()
+int main(void)
 {
-    int i;
-    for (i=97; i<=122; i++){
-        putchar(i);
-    }
-    return 0;
+int i;
+
+for (i = 97; i <= 122; i++)
+  putchar(i);
+return (0);
 }
