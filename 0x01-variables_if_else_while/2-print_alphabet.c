96bd3656aed1479199bccc36fae9a5e85b80b8a0
Amir Hamdi <am3566146@gmail.com>
2023-03-17 02:21:26 -0700

    qq

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main=Entry point
-*
-*this program prints lowercase alph
-*
-*/
-
+ * main = Entry point
+ *
+ * Description:  the program prints the lowercase alph
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i;
