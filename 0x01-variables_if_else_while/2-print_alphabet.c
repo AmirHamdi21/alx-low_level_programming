442d7e5012569af6f35e9a695486bd5628482a1c
Amir Hamdi <am3566146@gmail.com>
2023-03-17 02:05:42 -0700

    wqq

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main=Entry point
+*
+*this program prints lowercase alph
+*
+*/
+
+int main()
+{
+    int i;
+    for (i=97; i<=122; i++){
+        putchar(i);
+    }
+    return 0;
+}
