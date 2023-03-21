fe1e7a0289b080171b9e9f4ba9b6df2ad162bb1f
Amir Hamdi <am3566146@gmail.com>
2023-03-21 02:50:07 -0700

    ssA

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,9 @@
+/**                                                                                                                                                                                 
+ * main - check the code                                                                                                                                                            
+ *                                                                                                                                                                                  
+ * Description:  the program prints _putchar                                                                                                                                        
+ *                                                                                                                                                                                  
+ * Return: Always 0 (Success)                                                                                                                                                       
+ */
 int _putchar(char c);
 void print_alphabet(void);
