35bb26cab4965eea02bdd6f85017077f952ccde7
Amir Hamdi <am3566146@gmail.com>
2023-03-21 05:03:27 -0700

    aaa

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,19 @@ void times_table(void)
 int i,j,s;
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; j <= 9; j++)
+_putchar('0');
+for (j = 1; j <= 9; j++)
 {
-s = i * j;
-printf("%d", s);
-if (j != 9)
 _putchar(',');
-_putchar('  ');
+_putchar(' ');
+s = i * j;
+if (s <= 9)
+_putchar(' ');
+else
+{
+_putchar(s / 10 + '0');
+}
+_putchar(s % 10 + '0');
 }
 if (i != 9)
 _putchar('\n');
