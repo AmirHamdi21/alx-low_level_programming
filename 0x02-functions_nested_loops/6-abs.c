db14e20cf7b1ca2cc0c543fbc43788ed9188a238
Amir Hamdi <am3566146@gmail.com>
2023-03-21 04:04:38 -0700

    aaa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
+*/
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else if (n < 0)
+return (n*-1);
+else
+return (0);
+}
