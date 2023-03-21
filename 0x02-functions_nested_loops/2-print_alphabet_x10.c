9c2341b7e2aa0b9119fbf763ae3c94f7374386a7
Amir Hamdi <am3566146@gmail.com>
2023-03-21 00:43:18 -0700

    ss

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  the program prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+print_alphabet_x10();
+
+return (0);
+}
+void print_alphabet_x10(void){
+int c = 'a';
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c < 'z'; c++)
+putchar(c);
+putchar('\n');
+}
+
+return;
+}
