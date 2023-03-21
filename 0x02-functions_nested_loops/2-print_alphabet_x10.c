12882b52f68e7ac1727948ad30467bb95ad5b7b6
Amir Hamdi <am3566146@gmail.com>
2023-03-21 03:08:51 -0700

    aa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,6 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-print_alphabet_x10();
-
-return (0);
-}
 void print_alphabet_x10(void){
 int c = 'a';
 int i = 0;
