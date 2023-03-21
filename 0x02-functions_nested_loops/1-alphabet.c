b4e336ad0567132820ab2df3930dbd986bc2accd
Amir Hamdi <am3566146@gmail.com>
2023-03-21 02:56:21 -0700

    aa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)  /* Description:  the program prints _putchar */ 
+/**
+ * Prints the alphabet in lowercase, followed by a newline.
+ */
+void print_alphabet(void) 
 {
 int c = 'a';
 for (c = 'a'; c <= 'z'; c++)
