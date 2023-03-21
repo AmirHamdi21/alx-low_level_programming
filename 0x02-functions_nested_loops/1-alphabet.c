195e54f19c2f40d3ddd847aa57f01789b3953732
Amir Hamdi <am3566146@gmail.com>
2023-03-21 03:05:40 -0700

    ss

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
  * Description: print_alphabet Prints alphabet in lowercase
- *
- * Return: Always 0 (Success)
- *
  */
 void print_alphabet(void)
 {
