c686aab71aa626b5bf54cfce4e78ad69db0263ff
Amir Hamdi <am3566146@gmail.com>
2023-03-21 03:14:53 -0700

    ss

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  the program prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return 1;
+else
+return 0;
+}
+
