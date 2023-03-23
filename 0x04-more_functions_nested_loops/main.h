666e77cb6cb5fceccd9dc3c731c98ddd1d32cea1
Amir Hamdi <am3566146@gmail.com>
2023-03-23 07:19:18 -0700

    www

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/**                                                                                                                                                                                 
+ * main - check the code                                                                                                                                                            
+ *                                                                                                                                                                                  
+ * Description:  the program prints _putchar                                                                                                                                        
+ *                                                                                                                                                                                  
+ * Return: Always 0 (Success)                                                                                                                                                       
+ */ 
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
