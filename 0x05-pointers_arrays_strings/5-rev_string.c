4eaa04900d007873c752d7505691f04f09c80b98
Amir Hamdi <am3566146@gmail.com>
2023-03-27 14:45:48 -0700

    qqq

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+int len = strlen(s);
+len = len - 1;
+int i, j, temp, k = len;
+for (i = 0; i < k; i++)
+{
+for (j = 0; j < len; j++)
+{
+temp = s[j];
+s[j] = s[j+1];
+s[j+1] = temp;
+}
+len = len -1;
+}
+}
