9d8b062789a7399ccb96284d3751d55e9cb87149
Amir Hamdi <am3566146@gmail.com>
2023-03-29 17:20:56 -0700

    qqq

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,22 @@ char *leet(char *s)
 {
 	int i;
 	int j;
+	int k;
 
 	char l[] = "oOlLeEaAtT";
 	char e[] = "0011334477";
 
-	i = -1;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
+		k = 0;
 		while (l[j] != '\0')
 		{
 			if (s[i] == l[j])
 			{
-				s[i] = e[j];
+				k = j;
+				s[i] = e[k];
 			}
 			j++;
 		}
